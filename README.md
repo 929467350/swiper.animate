4f2405f02103e469dc183406ae9c25955afa353c
929467350 <>
2020-01-21 09:30:33 +0800

    update

--- README.md
+++ README.md
@@ -1,2 +1,60 @@
 # swiper.animate
- 
+<p>
+demo：<a href='https://929467350.github.io/swiper.animate/example.html'>传送门</a>
+</p>
+<p>
+1.引入模块
+</p>
+<pre>
+<code>
+let swiperAnimate=window.swiperAnimate;
+let swiperAnimateCache=window.swiperAnimateCache;
+</code>
+</pre>
+<p>
+2.初始化
+</p>
+<pre>
+<code>
+let mySwiper = new Swiper ('.swiper-container', {
+    on:{
+      init: function(){
+        swiperAnimateCache(this); //隐藏动画元素 
+        swiperAnimate(this); //初始化完成开始动画
+      }, 
+      slideChange: function(){ 
+        swiperAnimate(this); //每个slide切换结束时也运行当前slide动画
+      } 
+    }
+  })       
+</code>
+</pre>
+3.在需要运动的元素上面增加类名 ani ，swiper.animate需要指定几个参数：
+<ul>
+<li>value：    切换效果，例如 fadeInUp</li>
+<li>delay：    动画延迟时间，例如 0.3s</li>
+<li>loop：     动画循环次数，例如 1 (0为无限次)</li>
+<li>duration： 动画持续时间，例如 0.5s</li>
+</ul>
+</p>
+<pre>
+<code>
+&lt;div class="swiper-slide"&gt;
+&lt;p class="ani" swiper-animate-effect='[{"value":"fadeInLeft","duration":1,"loop":1,"delay":0}]'&gt;内容 &lt;/p&gt;
+&lt;/div&gt;
+
+       // 动态设置动画
+        let ani = [{"value": "fadeInRight","duration": 1,"loop": 1,"delay": 0}];
+        for (let i = 0; i < 3; i++) {
+            let div = document.createElement('div');
+            let p = document.createElement('p');
+            p.innerText = '这是一个新的Slide';
+            p.className = 'ani';
+            p.setAttribute('swiper-animate-effect', JSON.stringify(ani));
+            div.appendChild(p);
+            div.className = 'swiper-slide';
+            mySwiper.addSlide(1, div);
+            mySwiper.updateSlides();
+        }
+</code>
+</pre>
---
+++ swiper.animate.js
@@ -0,0 +1,79 @@
+/**
+ * @author      孟繁波
+ * @variation   1.0.0
+ * @qq          929467350
+ * @date        2020年1月20日
+ * @github      https://github.com/929467350/
+ * @example     https://929467350.github.io/swiper.animate/example.html
+ * @description 基于swiper.js 用于单个节点添加多个动画 支持循环
+ */
+
+// 缓存样式
+const swiperAnimateCache = (swiper) => {
+    for (let i = 0; i < swiper.slides.length; i++) {
+        for (let slides = swiper.slides[i].querySelectorAll('.ani'), j = 0; j < slides.length; j++) {
+            slides[j].attributes['style'] ? slides[j].setAttribute('swiper-animate-style-cache', slides[j].attributes['style'].value) :
+                (slides[j].setAttribute('swiper-animate-style-cache', ''), slides[j].style.visibility = 'hidden');
+        }
+    }
+}
+// 动画执行完毕重置当前节点样式清除动画
+const clearSwiperAnimate = (el, swiper) => {
+    let ani = el.getAttribute('effect');
+    let style = el.getAttribute('swiper-animate-style-cache');
+    ani && el.classList.remove(ani), el.removeAttribute('effect');
+    el.setAttribute("style", style);
+    el.style.visibility = "visible";
+}
+
+const swiperAnimate = (swiper) => {
+    // 初始化或页面发生变化时清除所有待执行节点动画并且隐藏节点
+    for (let ani = document.querySelectorAll('.ani'), j = 0; j < ani.length; j++) {
+        ani[j].style['visibility'] = "hidden";
+        // 如果动画是无限循环，则不会触发下方回调 所以页面发生变化时清除animation-duration和animation-iteration-count
+        ani[j].style['animation-duration'] = '';
+        ani[j].style['animation-iteration-count'] = '';
+    }
+    const animation = async (element, animations) => {
+        const run = (el, animation) => {
+            return new Promise(resolve => {
+                let effect = animation.value;
+                el.classList.add(effect);
+                el.setAttribute('effect', effect);
+                let style = el.getAttribute('style');
+                let duration = animation.duration;
+                let delay = animation.delay;
+                let loop = animation.loop;
+                if (loop === 0 || loop === '0') {
+                    loop = 'infinite';
+                }
+                duration && (style = style + "animation-duration:" + duration + "s;-webkit-animation-duration:" + duration + "s;")
+                delay && (style = style + "animation-delay:" + delay + "s;-webkit-animation-delay:" + delay + "s;")
+                loop && (style = style + "animation-iteration-count:" + loop + ";-webkit-animation-iteration-count:" + loop + ";")
+                el.setAttribute("style", style);
+                // 监听动画执行完毕函数
+                let resolveFn = () => {
+                    el.removeEventListener('animationend', resolveFn, false);
+                    el.removeEventListener('animationcancel', resolveFn, false);
+                    clearSwiperAnimate(el, swiper);
+                    resolve();
+                }
+                el.addEventListener('animationend', resolveFn, false)
+                el.addEventListener('animationcancel', resolveFn, false);
+            })
+        }
+        // 让动画跑起来 如果存在多个动画，会等待上次动画执行完毕后继续下个动画;
+        for (let i = 0, len = animations.length; i < len; i++) {
+            await run(element, animations[i]);
+        }
+    }
+    var node = swiper.slides[swiper.activeIndex].querySelectorAll(".ani");
+    node.forEach(element => {
+        element.style.visibility = "visible";
+        let ani = JSON.parse(element.attributes["swiper-animate-effect"].value);
+        animation(element, ani);
+    });
+};
+
+window.swiperAnimate = swiperAnimate;
+window.swiperAnimateCache = swiperAnimateCache;
